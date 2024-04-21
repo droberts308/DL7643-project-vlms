3e389f0d4858e07cc10bfcef0d2b52efa5b9cd69
droberts308 <droberts308@gatech.edu>
2024-04-21 17:46:47 -0400

    Change bert mbert

--- modules/denisa_vlm_reasoners/text_encoder.py
+++ modules/denisa_vlm_reasoners/text_encoder.py
@@ -1,4 +1,3 @@
-
 import os
 import pdb
 
@@ -6,12 +5,7 @@ import nltk
 import numpy as np
 import torch
 
-import puzzle_utils
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-SEED = 0
+import utils
 
 
 class BERT:
@@ -20,8 +14,10 @@ class BERT:
         super(BERT, self).__init__()
         from transformers import BertModel, BertTokenizer
 
-        self.model = BertModel.from_pretrained("bert-base-uncased").to(device)
-        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        self.model = BertModel.from_pretrained("bert-base-multilingual-cased").to(
+            "cuda"
+        )
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
         self.word_dim = 768
 
     def get_word_dim(self):
@@ -30,12 +26,11 @@ class BERT:
     def word_embed(self, sentence):
         with torch.no_grad():
             inputs = self.tokenizer(sentence, return_tensors="pt", padding=True).to(
-                device
+                "cuda"
             )
             outputs = self.model(**inputs)
-            word_feats = outputs.last_hidden_state
-            res = torch.tensor(word_feats.squeeze()).to(device)
-        return res
+            word_reprs = outputs.last_hidden_state
+        return torch.tensor(word_reprs.squeeze()).cuda()
 
 
 def globals_init(args):
@@ -46,6 +41,7 @@ def globals_init(args):
     global puzzles_not_included, num_actual_puzz
     global PS_VAL_IDX, PS_TEST_IDX
 
+    device = "cuda"
     puzzle_diff = {"easy": ""}  # {'easy': 'e', 'medium': 'm', 'hard': 'h'}
     puzzle_diff_str = {"easy": ""}
     osp = os.path.join
@@ -54,15 +50,13 @@ def globals_init(args):
     MAX_DECODE_STEPS = 10  # number of steps to decode the LSTM.
     num_puzzles = 101
     max_qlen = 110
-    seed = SEED
-    icon_dataset_path = "/home/hice1/droberts308/ai-dlr/vlm_reasoners/dataset/icon-classes.txt"
-    icon_class_ids = puzzle_utils.get_icon_dataset_classes(
-        icon_dataset_path
-    )  # os.listdir(icon_dataset_path) # puzzle 1
+    seed = 10
+    icon_dataset_path = "./dataset/icon-classes.txt"
+    icon_class_ids = utils.get_icon_dataset_classes(icon_dataset_path)
     signs = np.array(["+", "-", "x", "/"])  # puzzle 58
     NUM_CLASSES_PER_PUZZLE = {}
     SEQ_PUZZLES = [16, 18, 35, 39, 63, 100]
-    SMART_DATASET_INFO_FILE = "/home/hice1/droberts308/ai-dlr/vlm_reasoners/dataset/SMART_info_v2.csv"
+    SMART_DATASET_INFO_FILE = "./dataset/SMART_info_v2.csv"
     num_actual_puzz = 102
     puzzles_not_included = set([])
     PS_VAL_IDX = [7, 43, 64]
